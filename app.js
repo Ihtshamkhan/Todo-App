164087111061b8de8424abb0982e06f1d0ee547c
ihtshamkhan <ihtshamkhan88@gmail.com>
2024-11-02 12:55:51 +0500

    first commit

--- app.js
+++ app.js
@@ -1,34 +1,43 @@
 const list = document.querySelector("#todo-list")
 const form = document.querySelector("#todo-form")
 
-
-form.addEventListener('submit',(event)=>{
-    event.preventDefault()
-
-    const todo = event.target.children[0].value;
-
-    list.innerHTML += `<li> ${todo} </li>`;
-
-    if (todo === "") {
-       alert("Enter a List")
-       
-    }
-
-  
-    event.target.reset();
-
-}) 
-
-const todoList = [
+var todoList = [
     "Karachi",
     "Lahore",
     "Islamabad",
 ];
 
+if (!localStorage.getItem("List")) {
+    localStorage.setItem("List", JSON.stringify(todoList));
+
+}
+var todoList = JSON.parse(localStorage.getItem("List"));
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const todo = event.target.children[0].value;
+
+    var array = JSON.parse(localStorage.getItem("List"));
+    
+array.push(todo);
+localStorage.setItem("List", JSON.stringify(array));
+
+    list.innerHTML += `<li> ${todo} </li>`;
+
+    if (todo === "") {
+        alert("Enter a List")
+    }
+    event.target.reset();
+})
+
+
 // for (let i = 0; i < todoList.length; i++) {
-    // list.innerHTML += `<li> ${todoList[i]}</li>`  
+// list.innerHTML += `<li> ${todoList[i]}</li>`  
 // }
 
 todoList.map(function (value) {
     list.innerHTML += `<li> ${value}</li>`
 })
+
